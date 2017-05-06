4bd231b8d7d8458f23408e84c8d78cf09a5e2f37
Erik Andersen Neto <erikneto@br.ibm.com>
2017-05-06 17:56:57 -0300

    Correção de bugs. Inserção das intenções alternativas no resultado

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var watsontest = require("./node_modules/conversationtest/watsontest");
 var user = "e51d5555-bbce-44c7-b51c-fafb7d037293";
 var pwd = "ctQFAgPKJqop";
 //var workspace = "1a9b91ef-204c-4ee6-a63a-349fc2391615" //teste pessoal
-var workspace = "bca09dc6-2a79-4bc2-966f-8124bda53846";
-
-
+var workspace = "29e722fb-3de9-491d-a94f-ddb370c6ce72";
 
 watsontest.init(user,pwd,workspace);
 
-watsontest.runTest();
+watsontest.runTest((err,result)=>{
+
+});
 
