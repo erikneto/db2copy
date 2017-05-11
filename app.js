1441068018476257674eb999a69aeaecb48f8ea4
Erik Andersen Neto <erikneto@br.ibm.com>
2017-05-11 14:56:19 -0300

    updates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var watsontest = require("./node_modules/conversationtest/watsontest");
 var user = "e51d5555-bbce-44c7-b51c-fafb7d037293";
 var pwd = "ctQFAgPKJqop";
 //var workspace = "1a9b91ef-204c-4ee6-a63a-349fc2391615" //teste pessoal
-var workspace = "29e722fb-3de9-491d-a94f-ddb370c6ce72";
+var workspace = "1889be20-115e-4882-9396-bd84d7af2de9";
 
 watsontest.init(user,pwd,workspace);
 
