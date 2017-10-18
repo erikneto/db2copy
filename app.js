f8d2da78c968eb71548dc81ec5dd571f575073d1
Erik Andersen Neto <erikneto@br.ibm.com>
2017-10-18 14:32:48 -0200

    melhorias no teste

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var watsontest = require("./node_modules/conversationtest/watsontest");
 var user = "e51d5555-bbce-44c7-b51c-fafb7d037293";
 var pwd = "ctQFAgPKJqop";
 //var workspace = "1a9b91ef-204c-4ee6-a63a-349fc2391615" //teste pessoal
-var workspace = "cc288362-badf-4e1d-9e90-d6a3d09ac8fb";
+var workspace = "57dece2a-4926-43ed-b6e6-ed0b5deb4da4";
 
 watsontest.init(user,pwd,workspace);
 
